d6e67317dd5464b377b4dfe65ab39c6bce0f635a
haruanri <nanteneus@hotmail.co.jp>
2018-10-13 20:17:09 +0900

    電子書籍一覧表示機能

---
+++ DigitalLibraryApplication/Controllers/Welcome.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DigitalLibraryApplication.Controllers
+{
+    public class Welcome : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to Digital Library!";
+            return View();
+        }
+
+        public string Description()
+        {
+            return "This is the application for managing digital books!";
+        }
+    }
+}
---
+++ DigitalLibraryApplication/Models/Books.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigitalLibraryApplication.Models
+{
+    public class Books
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+    }
+}
